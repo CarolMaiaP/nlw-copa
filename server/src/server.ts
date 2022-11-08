51124836805ccc3500321acc7453361211ea6660
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-11-08 15:48:53 -0300

    feat: Return data from the pool table where code starts with T

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,24 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   })
 
-  fastify.get('/pools/count', () => {
-    return { count: 2548373 }
+  fastify.get('/pools/count', async () => {
+    const pools = await prisma.pool.findMany({
+      where: {
+        code: {
+          startsWith: 'T'
+        }
+      }
+    })
+    return { pools }
   })
 
   await fastify.listen({ port: 3333 })
