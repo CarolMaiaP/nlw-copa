479c43b9040889861051444b97ea5087c26a3e94
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-11-29 10:55:38 -0300

    feat: Function createPool added

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,6 +4,7 @@ import userAvatarExample from '../assets/user-avatar-example.png'
 import iconCheckImage from '../assets/icon-check.svg'
 import appPreviewImg from '../assets/nlw-copa-preview.png'
 import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react'
 
 interface HomeProps {
   poolCount: number;
@@ -12,6 +13,27 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
+  const [ poolTitle, setPoolTitle  ] = useState('');
+
+  async function createPool(event: FormEvent){
+    event.preventDefault();
+
+    try{
+      const response = await api.post('/pools',{
+        title: poolTitle,
+      });
+
+      const { code } = response.data
+
+      await navigator.clipboard.writeText(code)
+
+      alert('Bolão criado com sucesso, o código foi copiado para a área de transferência!')
+    }catch(err){
+      console.log(err);
+      alert('Falha ao criar o bolão, tente novamente!')
+    }
+  }
+
   return (
     <div className="max-w-[1124px] h-screen  mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
@@ -24,11 +46,12 @@ export default function Home(props: HomeProps) {
            pessoas já estão usando</strong>
         </div>
 
-        <form className="mt-10 flex gap-2">
+        <form onSubmit={createPool} className="mt-10 flex gap-2">
           <input 
-            className="flex-1 px-6 py-4 rounded border border-gray-600 bg-gray-800 text-sm"
+            className="flex-1 px-6 py-4 rounded border border-gray-600 bg-gray-800 text-sm text-gray-100"
             type="text" 
-            required placeholder='Qual nome do seu bolão?' 
+            required placeholder='Qual nome do seu bolão?'
+            onChange={event => setPoolTitle(event.target.value)} 
           />
           <button 
             className="bg-yellow-500 px-6 py-4 text-sm rounded font-bold text-gray-900 uppercase hover:bg-yellow-700"
